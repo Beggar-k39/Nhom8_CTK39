822f9583566c77074ec44b7fad6596dee25fff53
Nguyễn Dương Quốc Thái <quocthai0099@gmail.com>
2019-03-20 14:29:44 +0700

    Update Master Page

---
+++ Online_Education/Online_Education/Content/Front_End/js/custom_js.js
@@ -0,0 +1,20 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var maintop = document.querySelector('.main-top'),
+        shortmenu = document.querySelector('.short-menu');
+    
+    var status = true;
+    window.addEventListener('scroll', function () {
+        if (this.window.pageYOffset > 94) {
+            if (status) {
+                maintop.classList.add('menu-hide');
+                shortmenu.classList.add('short-menu-show');
+                status = false;
+            }
+        } else {
+            maintop.classList.remove('menu-hide');
+            shortmenu.classList.remove('short-menu-show');
+            status = true;
+        }
+    })
+    
+},false)
