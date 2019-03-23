b4c8750907eb4398d8b1dd8f2d5e6114c29f84a3
QuocThai <quocthai0099@gmail.com>
2019-03-23 11:58:26 +0700

    Update custom_js.js

--- Online_Education/Online_Education/Content/Front_End/js/custom_js.js
+++ Online_Education/Online_Education/Content/Front_End/js/custom_js.js
@@ -14,10 +14,12 @@
                 shortmenu.classList.add('short-menu-show');
                 status = false;
             }
-        } else {
-            maintop.classList.remove('menu-hide');
-            shortmenu.classList.remove('short-menu-show');
-            status = true;
+        } else if (y <= 94) {
+            if (!status) {
+                maintop.classList.remove('menu-hide');
+                shortmenu.classList.remove('short-menu-show');
+                status = true;
+            }
         }
 
         if (y > 200) {
@@ -25,9 +27,11 @@
                 stickyleft.classList.add('sticky-left-show');
                 status1 = false;
             }
-        } else {
-            stickyleft.classList.remove('sticky-left-show');
-            status1 = true;
+        } else if (y<= 200) {
+            if (!status) {
+                stickyleft.classList.remove('sticky-left-show');
+                status1 = true;
+            }
         }
 
     })
